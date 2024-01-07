483a7c935b886431d3a2ae2d155c22994d74fac3
ErzaaGashi <erzzag15@gmail.com>
2024-01-07 16:27:28 +0100

    Connected with MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
 // import express from "express";
 
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use("/api", (req,res,next) =>{
-    res.send("Hello");
+    res.send("Hello here!!!");
 });
 
-app.listen(5000);
+mongoose.connect('mongodb+srv://erzzag15:eg1120@cluster0.cwaskue.mongodb.net/').then(()=>
+    app.listen(5000)).then(()=>console.log("Connected to Database and listing to localhost 5000"))
+    .catch((err)=> console.log(err));
